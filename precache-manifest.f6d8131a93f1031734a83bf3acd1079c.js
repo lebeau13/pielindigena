1d9afe4965312b6104f43a8b5c1c37eacb4cf7d4
Lebeau <cesaran13@hotmail.com>
2020-07-15 10:24:33 -0500

    Updates

---
+++ precache-manifest.f6d8131a93f1031734a83bf3acd1079c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d3a1e4151dd575158f723b24dbe2c0",
+    "url": "/pielindigena/index.html"
+  },
+  {
+    "revision": "414c3cb3484856188cc3",
+    "url": "/pielindigena/static/css/main.67c26755.chunk.css"
+  },
+  {
+    "revision": "9a6e057df6d3e6bd8d3d",
+    "url": "/pielindigena/static/js/2.70b1d41e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pielindigena/static/js/2.70b1d41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414c3cb3484856188cc3",
+    "url": "/pielindigena/static/js/main.970453e5.chunk.js"
+  },
+  {
+    "revision": "e9eb5c76c0704ef1b05d",
+    "url": "/pielindigena/static/js/runtime-main.6ec6ccfc.js"
+  }
+]);
